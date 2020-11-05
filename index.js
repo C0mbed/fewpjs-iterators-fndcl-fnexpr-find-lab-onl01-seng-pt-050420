92d1339325c0e8639c6f34bd495a958f71921ab2
C0mbed <matt@farncombe.co>
2020-11-05 13:35:10 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(input) {
+  const result = input.find( ({result}) => result === "W")
+  if (result === undefined) {
+    return undefined;
+  } else {
+    return result['year'];
+  }
+};
